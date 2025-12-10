d5ce98f5ef856145359db37801eb12ef7a8ffaf1
Rodrigo_Fernandes <55065897+MonoPT@users.noreply.github.com>
2025-12-10 20:09:19 +0000

    card

--- front-end/src/lib/composables/mediaVisualizer.svelte
+++ front-end/src/lib/composables/mediaVisualizer.svelte
@@ -20,8 +20,26 @@
             {/each}
         </div>
         
-        <div class="data-wrapper card">
-            <h2><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-forms "><path d="M12 3a3 3 0 0 0 -3 3v12a3 3 0 0 0 3 3"></path><path d="M6 3a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3"></path><path d="M13 7h7a1 1 0 0 1 1 1v8a1 1 0 0 1 -1 1h-7"></path><path d="M5 7h-1a1 1 0 0 0 -1 1v8a1 1 0 0 0 1 1h1"></path><path d="M17 12h.01"></path><path d="M13 12h.01"></path></svg>Generation data</h2>
+        <div class="card">
+            <h2>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-forms "><path d="M12 3a3 3 0 0 0 -3 3v12a3 3 0 0 0 3 3"></path><path d="M6 3a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3"></path><path d="M13 7h7a1 1 0 0 1 1 1v8a1 1 0 0 1 -1 1h-7"></path><path d="M5 7h-1a1 1 0 0 0 -1 1v8a1 1 0 0 0 1 1h1"></path><path d="M17 12h.01"></path><path d="M13 12h.01"></path></svg>
+                Generation data
+            </h2>
+        </div>
+        
+        <div class="card">
+            <h2>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-chart-bubble "><path d="M6 16m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path><path d="M16 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path><path d="M14.5 7.5m-4.5 0a4.5 4.5 0 1 0 9 0a4.5 4.5 0 1 0 -9 0"></path></svg>
+                Process
+            </h2>
+            <h3>Tools</h3>
+            <div class="cardWrapper">
+                <span>GPT Image 1</span>
+            </div>
+            <h3>Techniques</h3>
+            <div class="cardWrapper transparentTag">
+                <span>Text2IMG</span>
+            </div>
         </div>
     </div>
 </div>
@@ -37,8 +55,10 @@
         width: 100%;
         min-height: 100px;
         border-radius: .4rem;
-        
+                
         h2 {
+            padding-top: 0;
+             padding-bottom: calc(var(--spacing) * 4);
             display: flex;
             align-items: center;
             gap: .4rem;
@@ -51,7 +71,7 @@
         }
     }
     
-    .tags {
+    .tags, .cardWrapper {
         display: flex;
         flex-wrap: wrap;
         gap: calc(var(--spacing) * 2);
@@ -62,6 +82,29 @@
             border-radius: calc(var(--spacing) * .7);
             padding: calc(var(--spacing) * 1.5);
             padding-inline: calc(var(--spacing) * 4);
+        }
+    }
+    
+    .cardWrapper {
+        padding-top: calc(var(--spacing) * 2);
+        padding-bottom: calc(var(--spacing) * 4);
+        
+        span {
+            background: #1971C2;
+            border-radius: 1000px;
+            border: 1px solid color-mix(in srgb, #1971C2 60%, #fff 40%);
+            box-sizing: content-box;
+            color: rgba(255,255,255,.85);
+            font-size: .9em;
+        }
+        
+        &.transparentTag {
+            gap: 2rem;
+            span {
+                padding: 0;
+                border: none;
+                background: transparent;
+            }
         }
     }
     
