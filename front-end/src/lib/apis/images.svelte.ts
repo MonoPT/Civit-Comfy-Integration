16b9d5136367c456827f00099d10a58d51f6abc2
Rodrigo_Fernandes <55065897+MonoPT@users.noreply.github.com>
2026-02-01 02:08:20 +0000

    re implemented mansonary layout

---
+++ front-end/src/lib/apis/images.svelte.ts
@@ -0,0 +1,86 @@
+import API from "$lib/api";
+import {user_token} from "$lib/state.svelte"
+
+type return_assets = {
+  status: number,
+  assets: object[]
+}
+
+export default class load_mansonary {
+  static cursor = ""
+  
+  static assets_stack = []
+  
+  static default_payload() {
+    return {
+      period: "Month",
+      sort: "Most Reactions",
+      baseModel: "",
+      mediaType: "Image",
+      cursor: load_mansonary.cursor,
+      browsingLevel: "31",
+      techniques: "",
+      requiringMeta: "false",
+      madeOnsite: "false",
+      originalsOnly: "false",
+      remixesOnly: "false",
+      tags: ""
+    }
+  }
+  
+  static async fetch_assets(): Promise<return_assets> {
+    let payload = load_mansonary.default_payload()
+    let endpoint = API.infinite_images(user_token.token, new URLSearchParams(payload).toString())
+    
+    let response = await fetch(endpoint)
+    
+    if (response.status === 200) {
+      let res = await response.json() 
+      
+      let images = (res[0] as object[]).map((asset_item) => {
+        //@ts-ignore
+        const original_image_url = asset_item.img_url
+        
+        // Replace otimization settings
+        
+        const new_url = original_image_url.replace("/transcode=true,original=true,quality=90/", "/anim=false,transcode=true,width=400,original=false,optimized=true/")
+        
+        //@ts-ignore
+        asset_item.optimized_img_url = new_url
+        
+        asset_item.ratio = getAspectRatio(asset_item.width || 0, asset_item.height || 0)
+        
+        return asset_item
+      })
+      load_mansonary.cursor = (res[1] as string).replaceAll("\"", "")
+      
+      return {status: response.status, assets: images}
+    }
+    
+    return {status: response.status, assets: []}
+  }
+}
+
+function getAspectRatio(
+  width: number,
+  height: number,
+  maxDepth = 50
+) {
+  const gcd = (a: number, b: number, depth: number): number => {
+    if (depth >= maxDepth) {
+      // fallback: stop reducing further
+      return a;
+    }
+    if (b === 0) {
+      return a;
+    }
+    return gcd(b, a % b, depth + 1);
+  };
+
+  const divisor = gcd(width, height, 0);
+
+  return {
+    w: width / divisor,
+    h: height / divisor
+  };
+}
--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,4 +1,52 @@
-<script lang="ts">    
+<script lang="ts">
+    import { onDestroy, onMount } from "svelte";
+    import imageLoader from "$lib/apis/images.svelte"
+    import { RegularMasonryGrid  as MasonryGrid, Frame } from '@masonry-grid/svelte'
     
+    let assets_list: any[] = []
+    
+    let is_loadings_assets = false;
+    
+    onMount(() => {     
+      load_assets_batch();
+      
+      window.addEventListener("scroll", handle_scroll)
+    })
+    
+    onDestroy(() => window.removeEventListener("scroll", handle_scroll))
+    
+    function handle_scroll(event: Event) {
+      
+      if (window.scrollY > (document.body.scrollHeight * .8 || document.body.scrollHeight - 3000)) {
+        load_assets_batch();
+      }
+      
+    }
+    
+    async function load_assets_batch() {
+      if (is_loadings_assets) return
+      
+      is_loadings_assets = true
+      
+      let resp = await imageLoader.fetch_assets();
+      
+      if (resp.status === 200) {
+        assets_list = [...assets_list, ...resp.assets]
+      }
+      
+      is_loadings_assets = false
+    }
 </script>
- 
+
+<MasonryGrid
+  frameWidth={400}
+  gap={10}
+>
+    {#each assets_list as asset}
+        <Frame width={asset.ratio.w} height={asset.ratio.h}>
+            <div class="container">
+                <img loading="lazy" src='{asset.optimized_img_url}' alt='Photo' />
+            </div>
+        </Frame>
+    {/each}
+</MasonryGrid>
