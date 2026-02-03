9671197a0e3308044b712ad4889008cfc6ccfc0a
Rodrigo_Fernandes <55065897+MonoPT@users.noreply.github.com>
2026-02-03 00:26:09 +0000

    rust tls

--- front-end/src/routes/downloads/columns.ts
+++ front-end/src/routes/downloads/columns.ts
@@ -85,7 +85,7 @@ export const columns: ColumnDef<Table>[] = [
                         ${info.base_model}
                     </span>
                     <span class="flex items-center gap-2 text-muted-foreground">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-icon lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
                         ${info.release_Date}
                     </span>
                 </div>
