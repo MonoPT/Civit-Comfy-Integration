42090d523372bdfc064ffcf0402e8f7e0b490d78
Rodrigo_Fernandes <55065897+MonoPT@users.noreply.github.com>
2026-02-05 21:22:30 +0000

    add to collections

--- front-end/src/lib/composables/collections_manager.svelte
+++ front-end/src/lib/composables/collections_manager.svelte
@@ -24,21 +24,28 @@
     onMount(() => window.addEventListener("openCollectionManager", async (e) => {
       //@ts-ignore
       const {item_id, collection_type}: {item_id: number, collection_type: string} = e.detail
+      
+      item_id_g = item_id
+      collection_type_g = collection_type
+      
       isOpen = false
       isOpen = true
       
       isLoading = true
-      item_id_g = item_id
-      collection_type_g = collection_type
+      
       collections = []
       in_collections = []
       
+      await update_collections(item_id, collection_type_g)
+      
+      isLoading = false
+    }))
+    
+    async function update_collections(item_id: number, collection_type: string) {
       const [res1, res2] = await Promise.all([
           fetch(API.collections_with_media(user_token.token, item_id)),
           fetch(API.get_collections_by_media_type(user_token.token, collection_type)),
       ]);
-      
-      isLoading = false
       
       if (res1.status !== 200 || res2.status !== 200) {
         toast.error("An error has ocurred. Try again.")
@@ -48,7 +55,27 @@
       collections = (await res2.json() as any[]).filter((coll) => coll.name !== "comfyui_civit_favorites")
       
       in_collections = await res1.json() as any[]
-    }))
+    }
+    
+    async function handle_submit() {
+      isLoading = true
+      
+      const collectionItems = Array.from(document.querySelectorAll<HTMLInputElement>("[data-collection-item]")).map((item) => {
+        const collection_id = parseInt(item.getAttribute("data-collection-item")!)
+        const checked = item.getAttribute("data-state") === "checked" ? true : false
+
+        return {collection_id, checked}
+      })
+      
+      await fetch(API.update_media_collections(user_token.token, item_id_g, 
+        collectionItems.filter((item) => item.checked).map((item) => item.collection_id),
+        collectionItems.filter((item) => !item.checked).map((item) => item.collection_id)
+      ))  
+      
+      await update_collections(item_id_g, collection_type_g)
+      
+      isLoading = false
+    }
 </script>
 
 <Dialog.Root open={isOpen}>
@@ -61,7 +88,7 @@
         </Dialog.Header>
         <div class="grid gap-4">
             {#if isLoading}
-                <div class="flex flex-col items-center py-12">
+                <div class="flex flex-col items-center py-6">
                     <Spinner />
                     <h2 class="pt-2">Loading</h2>
                 </div>
@@ -69,7 +96,7 @@
                 <div class="flex flex-col gap-2 py-0.5" style="max-height: 50vh; overflow-y: auto;">
                     {#each collections as collection}
                         <div class="flex items-center gap-3">
-                        <Checkbox checked={in_collections.find((coll) => coll.collectionId === collection.id)} value={collection.id} id="coll-item-{collection.id}" />
+                        <Checkbox data-collection-item={collection.id} checked={in_collections.find((coll) => coll.collectionId === collection.id) ? true : false} value={collection.id} id="coll-item-{collection.id}" />
                         <Label for="coll-item-{collection.id}" class="font-normal">{collection.name}</Label>
                         </div>
                     {/each}
@@ -78,7 +105,7 @@
         </div>
         <Dialog.Footer>
             <Dialog.Close class={buttonVariants({ variant: "outline" })}>Cancel</Dialog.Close>
-            <Button disabled={isLoading}>
+            <Button disabled={isLoading} onclick={handle_submit}>
                 {#if isLoading}
                     <Spinner />
                     Loading
